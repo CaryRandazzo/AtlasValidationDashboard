160c4b867c59adc91f17014d2d62f4e338d6bda8
theluigus <vf-17@protonmail.com>
2022-04-06 01:11:40 -0500

    update volume path

--- AtlasValidationDashboard/docker-server/cary-server/server/app.py
+++ AtlasValidationDashboard/docker-server/cary-server/server/app.py
@@ -13,7 +13,7 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-import plotly.express as px
+# import plotly.express as px
 
 
 #############################
