24ac7256c2f512aecab3af5b7356c32e7a78d21f
Cary <56490230+CaryRandazzo@users.noreply.github.com>
2022-04-11 14:41:46 -0500

    Delete AtlasValidationDashboard/AVD_v2 directory

--- AtlasValidationDashboard/AVD_v2/config.py
+++
@@ -1,7 +0,0 @@
-#########################
-#Configuration File#
-#########################
-
-#configuring the plots to compare with chi2 - set the first and second files to compare
-fileOne = 'PHYSVAL_JZ3W_FE.root'
-fileTwo = 'PHYSVAL_JZ3W_PFO.root'
