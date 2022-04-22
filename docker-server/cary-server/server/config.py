568a67c5df554444c47bc3706cde6974558e4bbf
Cary <56490230+CaryRandazzo@users.noreply.github.com>
2022-04-22 11:37:14 -0500

    Update config.py
    remove pathing from file name

--- docker-server/cary-server/server/config.py
+++ docker-server/cary-server/server/config.py
@@ -3,8 +3,8 @@
 #####################
 
 # These are the two files that you want to compare
-fileOne = '/app/data/data15_13TeV.00276689.physics_Main.merge.HIST.f1051_h335._0001.1'
-fileTwo = '/app/data/data15_13TeV.00276689.physics_Main.merge.HIST.f1052_h335._0001'
+fileOne = 'data15_13TeV.00276689.physics_Main.merge.HIST.f1051_h335._0001.1'
+fileTwo = 'data15_13TeV.00276689.physics_Main.merge.HIST.f1052_h335._0001'
 
 # This list contains the .root directories that you are interested in for the comparison
 folder_list = ['CaloMonitoring', 'Jets','MissingEt','Tau','egamma']
