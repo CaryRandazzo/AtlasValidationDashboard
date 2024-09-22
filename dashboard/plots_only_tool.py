d5991927019057a05f286ebc47f548c7c7417f6b
Cary <randazzocary@gmail.com>
2024-09-21 21:26:06 -0500

    remove irrelevant file, set todo for refactoring, added copyright and author info, fix --diff error that was happening on lxplus, updated requirements.txt

--- dashboard/plots_only_tool.py
+++ dashboard/plots_only_tool.py
@@ -1,19 +1,172 @@
-# Cary Randazzo - 9-12-2024, for the ATLAS Collaboration
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 CERN and the ATLAS Collaboration
+#
+# This script has been created for the benefit of the ATLAS experiment and is made available under the
+# terms of the GNU General Public License v3.0. A copy of this license can be found at http://www.gnu.org/licenses/.
+#
+# Author: Cary Randazzo <Cary.David.Randazzo@cern.ch> of Louisiana Tech University, 9-21-2024
+# under the guidance of Dr. Lee Sawyer <lee.sawyer@cern.ch> of Louisiana Tech University
+#
+# Description: This script processes ROOT histograms and calculates chi-squared values
+#              for validation purposes. It includes functions for normalizing histograms,
+#              calculating chi-squared values, and plotting the results.
+#
+# For usage instructions and examples, run "python plots_only_tool.py --help".
+#
+# Note: This script requires the ROOT, pandas, matplotlib, and seaborn libraries. It has been tested with conda.
 
-import ROOT
-import pandas as pd
-import os
-import matplotlib.pyplot as plt
 import argparse
-import replica_processor as rp
-import seaborn as sns
-import matplotlib.colors as mcolors
-import sys
 import logging
 from typing import Tuple, Optional, Dict, Any
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import seaborn as sns
+import ROOT
 
 # Configure the system for logging
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+# TODO: Refactor processHistML and hist_to_df, similar to how validate_hist has been refactored.
+def processHistML(tf,file,f_path,f_path_list, f_type_list, binNums,binNumsY, occupancies):  
+    """
+    Process ROOT runfile histogram to data.
+    """
+    
+    # Main loop
+    for key in tf.GetListOfKeys():    
+        input = key.ReadObj()
+
+        # Determine if the location in the file we are at is a directory
+        if issubclass(type(input),ROOT.TDirectoryFile):
+           
+            # Record the path of the directory we are looking in
+            try:
+                f_path = input.GetPath() 
+            except:
+                print("cant GetPath")
+
+            # Split the path by '/' so we can determine where we are in the folder structure        
+            try:
+                split_path = f_path.split("/")
+            except:
+                print('cant split_path')            
+            
+            
+            # Recursively go deeper into the file structure depending on the length of split_path
+            if 'run' in split_path[-1]:
+                # We are 2 directories deep, go deeper
+                f_path,f_path_list, f_type_list, binNums,binNumsY, occupancies = processHistML(input,file,f_path, f_path_list, f_type_list, binNums,binNumsY, occupancies)  
+            elif len(split_path) > 2 and any(folder in split_path for folder in ('CaloMonitoring', 'Jets','MissingEt','Tau','egamma')):                
+                # We are greater than 3 directories deep and these directories include the specified folders above, goo deeper
+                f_path, f_path_list, f_type_list, binNums,binNumsY, occupancies = processHistML(input,file,f_path, f_path_list, f_type_list, binNums,binNumsY, occupancies)     
+            else:
+                pass
+            
+            # Record the file_path that will result now that we are done with the current folder level
+            #  i.e. the folder path that results from going up a level in the directory
+            f_path = f_path.split('/')
+            f_path = '/'.join(f_path[:-1])
+                
+        elif issubclass(type(input), ROOT.TProfile):
+            
+            # Record te path of the directory we are looking in with the name of the hist file as part of the path
+            try:
+                f_path_tp = f_path + '/' + input.GetName()                
+            except:
+                print("cant GetPath3")
+            
+            # Get the part of f_path that follows the ':'
+            f_path_tp = f_path_tp.split(':')
+            if 'eos' in f_path_tp:
+                f_path_tp = f_path_tp[2][1:]
+            else:
+                f_path_tp = f_path_tp[1][1:]
+            
+            hist_file = file.Get(f_path_tp)
+            binsX = hist_file.GetNbinsX()                                    
+            
+            # Setup the 3 arrays for creating the dataframe
+            for binX in range(binsX+1):
+                f_path_list.append(f_path_tp)
+                binNum = hist_file.GetBin(binX)
+                binNums.append(binX)
+                binNumsY.append(None)
+                occupancies.append(hist_file.GetBinContent(binNum))
+                f_type_list.append('TProfile')
+            
+        elif issubclass(type(input),ROOT.TH2):
+
+            # Record the path of the directory we are looking in with the name of the hist file as part of the path
+            try:
+                f_path_th2 = f_path + '/' + input.GetName()                
+            except:
+                print("cant GetPath3")
+            
+            # Get the part of f_path that follows the ':'
+            f_path_th2 = f_path_th2.split(':')
+            if 'eos' in f_path_th2:
+                f_path_th2 = f_path_th2[2][1:]
+            else:
+                f_path_th2 = f_path_th2[1][1:]
+            
+            
+            hist_file = file.Get(f_path_th2)
+            binsX = hist_file.GetNbinsX()                        
+            binsY = hist_file.GetNbinsY()
+            
+            # Setup the 3 arrays for creating the dataframe
+            for binX in range(binsX+1):
+                for binY in range(binsY+1):
+                    f_path_list.append(f_path_th2)
+                    binNumXY = hist_file.GetBin(binX,binY)
+                    binNums.append(binX)
+                    binNumsY.append(binY)
+                    occupancies.append(hist_file.GetBinContent(binNumXY))
+                    f_type_list.append('TH2')
+                
+        elif issubclass(type(input),ROOT.TH1):
+            
+            # Record the path of the directory we are looking in with the name of the hist file as part of the path
+            try:
+                f_path_th1 = f_path + '/' + input.GetName()                
+            except:
+                print("cant GetPath2")
+
+            # Get the part of f_path that follows the ':'
+            f_path_th1 = f_path_th1.split(':')
+            if 'eos' in f_path_th1:
+                f_path_th1 = f_path_th1[2][1:]
+            else:
+                f_path_th1 = f_path_th1[1][1:]
+            
+            hist_file = file.Get(f_path_th1)
+            binsX = hist_file.GetNbinsX()            
+            
+            # Setup the 3 arrays for creating the dataframe
+            for binX in range(binsX+1):                
+                f_path_list.append(f_path_th1)
+                binNum = hist_file.GetBin(binX,0)                
+                binNums.append(binNum)
+                binNumsY.append(None)                
+                occupancies.append(hist_file.GetBinContent(binNum))
+                f_type_list.append('TH1')
+    
+    return f_path, f_path_list, f_type_list, binNums,binNumsY, occupancies
+
+def hist_to_df(path):
+    """
+    Converts ROOT histogram data from ProcessHistML() to a pandas dataframe.
+    """
+    
+    file = ROOT.TFile.Open(path)
+
+    f_path, f_path_list, f_type_list, binNums,binNumsY, occupancies = processHistML(file,file,'',[],[],[],[],[])
+    
+    return pd.DataFrame({'paths':f_path_list,'f_type':f_type_list, 'x':binNums,'y':binNumsY,'occ':occupancies})
 
 def normalize_histogram(hist: pd.DataFrame, ref_hist: pd.DataFrame, option: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Normalize histogram based on the selected option.
@@ -656,10 +809,10 @@ if __name__ == "__main__":
     
         Full Commands Examples:
             (When running the script locally)
-            python plots_only_tool_refactored.py --file data24_13p6TeV.00472943.physics_Main.merge.HIST.f1442_h464._0001.1 --ref data24_13p6TeV.00472943.physics_Main.merge.HIST.r15810_p6305.root --htype TH1 --overlay --hname run_472943/CaloMonitoring/TileCellMon_NoTrigSel/General/Summary/TIME_execute
-            python plots_only_tool_refactored.py --file data24_13p6TeV.00472943.physics_Main.merge.HIST.f1442_h464._0001.1 --ref data24_13p6TeV.00472943.physics_Main.merge.HIST.r15810_p6305.root --htype TH1 --diff --hname run_472943/CaloMonitoring/TileCellMon_NoTrigSel/General/Summary/TIME_execute
+            python plots_only_tool.py --file data24_13p6TeV.00472943.physics_Main.merge.HIST.f1442_h464._0001.1 --ref data24_13p6TeV.00472943.physics_Main.merge.HIST.r15810_p6305.root --htype TH1 --overlay --hname run_472943/CaloMonitoring/TileCellMon_NoTrigSel/General/Summary/TIME_execute
+            python plots_only_tool.py --file data24_13p6TeV.00472943.physics_Main.merge.HIST.f1442_h464._0001.1 --ref data24_13p6TeV.00472943.physics_Main.merge.HIST.r15810_p6305.root --htype TH1 --diff --hname run_472943/CaloMonitoring/TileCellMon_NoTrigSel/General/Summary/TIME_execute
         (When running the script on LXPLUS, assumed to be in the same directory as the script OR in a cernbox eos location)
-            python plots_only_tool_refactored.py --file /eos/home-c/crandazz/SWAN_projects/ATLAS_DQ_Dashboard/data24_13p6TeV.00472943.physics_Main.merge.HIST.f1442_h464._0001.1 --ref /eos/home-c/crandazz/SWAN_projects/ATLAS_DQ_Dashboard/data24_13p6TeV.00472943.physics_Main.merge.HIST.r15810_p6305.root --htype TProfile --folders CaloMonitoring Jets MissingEt Tau egamma --mode chi2 --perndf
+            python plots_only_tool.py --file /eos/home-c/crandazz/SWAN_projects/ATLAS_DQ_Dashboard/data24_13p6TeV.00472943.physics_Main.merge.HIST.f1442_h464._0001.1 --ref /eos/home-c/crandazz/SWAN_projects/ATLAS_DQ_Dashboard/data24_13p6TeV.00472943.physics_Main.merge.HIST.r15810_p6305.root --htype TProfile --folders CaloMonitoring Jets MissingEt Tau egamma --mode chi2 --perndf
         ''',
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
@@ -851,10 +1004,10 @@ if __name__ == "__main__":
             parser.error("--diff additionally requires --hname")
             
         print('Processing "file" histogram data...')
-        file_data = rp.hist_to_df(args.file)
+        file_data = hist_to_df(args.file)
         
         print('Processing "ref" histogram data...')
-        ref_data = rp.hist_to_df(args.ref)
+        ref_data = hist_to_df(args.ref)
         
         print("Plotting difference values...")
         plot_diffs(file_data, ref_data, args.hname, args.htype)
